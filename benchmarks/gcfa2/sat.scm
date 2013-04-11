47cd5b6f5f06f03a0fd5914ec48778f4a333db1c
Ilya Sergey <ilya.sergey@gmail.com>
2013-04-11 19:51:03 +0200

    benchmarks adapted (no newlines)

--- benchmarks/gcfa2/sat.scm
+++ benchmarks/gcfa2/sat.scm
@@ -1,14 +1,9 @@
-(define (println s)
-  (display s)
-  (newline))
-
 (define (phi x1 x2 x3 x4)
   (and (or x1 (not x2) (not x3))
        (or (not x2) (not x3))
        (or x4 x2)))
 
 (define (try f)
-  (println "trying")
   (or (f #t) (f #f)))
 
 (define (sat-solve-4 p)
@@ -19,5 +14,4 @@
                               (p n1 n2 n3 n4))))))))))
 
                         
-(display (sat-solve-4 phi))
-(newline)
+(sat-solve-4 phi)
